15414480b7fcaa38e16561c8ddfca6f58bcfd0b3
xNetting <71952348+xNetting@users.noreply.github.com>
2021-07-28 02:50:17 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
 using System;
 using System.Collections.Generic;
 
-namespace Netting_Code
+namespace Progam
 {
-    class Netting
+    class Post
     {
         static void Main(string[] args)
         {
