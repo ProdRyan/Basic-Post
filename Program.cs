415e26fe01b28e7a21bcdaa68380543d25b81860
xNetting <71952348+xNetting@users.noreply.github.com>
2021-07-28 02:34:42 -0300

    Commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+
+namespace Netting_Code
+{
+    class Netting
+    {
+        static void Main(string[] args)
+        {
+            string postText = Console.ReadLine();
+
+            Post post = new Post();
+            post.Text = postText;
+            post.ShowPost();
+
+        }
+    }
+
+    class Post
+    {
+        private string text;
+        
+        public Post() {            
+            Console.WriteLine("¡Nuevo Post!");
+        }
+
+        public void ShowPost()
+        {
+            Console.WriteLine(text);
+        }
+        
+        public string Text {
+            get {return text;}
+            set {text = value;}
+        }
+    }
+}
